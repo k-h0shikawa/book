1336f1abd176d8dc8ba85d7fc56991e7824273a4
k-h0shikawa <81114182+k-h0shikawa@users.noreply.github.com>
2025-06-13 15:54:39 +0900

    第11章 不要になったら消す
    ＫｅｎｔＢｅｃｋ. テスト駆動開発 (p.142). Kindle 版.

--- テスト駆動開発/app/src/main/java/com/sample/app/money/Dollar.java
+++
@@ -1,9 +0,0 @@
-package com.sample.app.money;
-
-public class Dollar extends Money {
-    
-    Dollar(int amount, String currency) {
-        super(amount, currency);
-    }
-
-}
--- テスト駆動開発/app/src/main/java/com/sample/app/money/Franc.java
+++
@@ -1,8 +0,0 @@
-package com.sample.app.money;
-
-public class Franc extends Money {
-
-    Franc(int amount, String currency) {
-        super(amount, currency);
-    }
-
