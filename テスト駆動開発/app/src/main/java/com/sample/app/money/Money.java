3f21ec46885dbd5d2f2f05fc31be453ad6acdb94
k-h0shikawa <81114182+k-h0shikawa@users.noreply.github.com>
2025-06-13 15:24:09 +0900

    第8章 実装を隠す
    ＫｅｎｔＢｅｃｋ. テスト駆動開発 (p.92). Kindle 版.

--- テスト駆動開発/app/src/main/java/com/sample/app/money/Money.java
+++ テスト駆動開発/app/src/main/java/com/sample/app/money/Money.java
@@ -1,11 +1,18 @@
 package com.sample.app.money;
 
-public class Money {
+abstract class Money {
     protected int amount;
-
+    abstract Money times(int multiplier);
     public boolean equals(Object object) {
         Money money = (Money) object;
         return this.amount == money.amount && 
                this.getClass().equals(money.getClass());
+    }
+
+    static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+    static Franc franc(int amount) {
+        return new Franc(amount);
     }
 }
